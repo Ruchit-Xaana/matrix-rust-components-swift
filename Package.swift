2d983e2a158190453b5d620f50fc543a3b4bb783
Ruchit Reddy <Ruchit.Lakku@xaana.ai>
2024-09-23 23:42:16 +1000

    Changed Package.swift file

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 let checksum = "49a8f12fafb7671486b6bbc9b6018a163552877d326cdcc7a7bd4642fbbd9d31"
 let version = "v1.0.44-gamma"
-let url = "https://github.com/element-hq/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
+let url = "https://github.com/Ruchit-Xaana/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
     name: "MatrixRustSDK",
     platforms: [
