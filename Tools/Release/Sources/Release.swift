91ac0c8322c66c7753df765cf2fd474c9187ba98
Ruchit-Xaana <ruchit.lakku@xaana.ai>
2024-09-23 07:04:54 +0530

    Added github workflow script to generate custom sdk

--- Tools/Release/Sources/Release.swift
+++ Tools/Release/Sources/Release.swift
@@ -6,6 +6,12 @@ import Foundation
 struct Release: AsyncParsableCommand {
     @Option(help: "The version of the package that is being released.")
     var version: String
+
+    @Option(help: "The branch of the source repository to build from.")
+    var branch: String
+
+    @Option(help: "The commit hash of the source repository to build from.")
+    var commitHash: String
     
     @Flag(help: "Prevents the run from pushing anything to GitHub.")
     var localOnly = false
@@ -14,19 +20,20 @@ struct Release: AsyncParsableCommand {
         .authorization(for: URL(string: "https://api.github.com")!)!
         .password
     
-    var sourceRepo = Repository(owner: "matrix-org", name: "matrix-rust-sdk")
-    var packageRepo = Repository(owner: "element-hq", name: "matrix-rust-components-swift")
+    var sourceRepo = Repository(owner: "Ruchit-Xaana", name: "matrix-rust-sdk")
+    var packageRepo = Repository(owner: "Ruchit-Xaana", name: "matrix-rust-components-swift")
     
-    var packageDirectory = URL(fileURLWithPath: #file)
-        .deletingLastPathComponent() // Release.swift
-        .deletingLastPathComponent() // Sources
-        .deletingLastPathComponent() // Release
-        .deletingLastPathComponent() // Tools
-    lazy var buildDirectory = packageDirectory
-        .deletingLastPathComponent() // matrix-rust-components-swift
-        .appending(component: "matrix-rust-sdk")
+    var packageDirectory: URL {
+        return URL(fileURLWithPath: "\(FileManager.default.currentDirectoryPath)/package")
+    }
+    lazy var buildDirectory: URL = {
+        return URL(fileURLWithPath: "\(FileManager.default.currentDirectoryPath)/source")
+    }
     
     mutating func run() async throws {
+        // Checkout the specified branch and commit
+        try checkoutBranchAndCommit()
+
         let package = Package(repository: packageRepo, directory: packageDirectory, apiToken: apiToken, urlSession: localOnly ? .releaseMock : .shared)
         Zsh.defaultDirectory = package.directory
         
@@ -39,13 +46,25 @@ struct Release: AsyncParsableCommand {
         try commitAndPush(package, with: product)
         try await package.makeRelease(with: product, uploading: zipFileURL)
     }
+
+    mutating func checkoutBranchAndCommit() throws {
+            let git = Git(directory: buildDirectory)
+
+            // Checkout the specified branch
+            try git.checkout(branch: branch)
+
+            // Checkout the specified commit
+            try git.checkout(commitHash)
+
+            Log.info("Checked out branch \(branch) at commit \(commitHash)")
+    }
     
     mutating func build() throws -> BuildProduct {
         let git = Git(directory: buildDirectory)
-        let commitHash = try git.commitHash
-        let branch = try git.branchName
-        
-        Log.info("Building \(branch) at \(commitHash)")
+        // Use the checked out commit hash and branch name
+        let currentCommitHash = try git.commitHash
+
+        Log.info("Building from commit \(currentCommitHash) on branch \(branch)")
         
         // unset fixes an issue where swift compilation prevents building for targets other than macOS
         let cargoCommand = "cargo xtask swift build-framework --release --target aarch64-apple-ios --target aarch64-apple-ios-sim --target x86_64-apple-ios"
